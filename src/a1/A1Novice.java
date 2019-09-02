ff016defea672e9203ee7e67ab1860a3432b83e6
boyce <boyce@DESKTOP-IQE6RLL>
2019-09-02 02:46:35 -0400

    Novice first attempt.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,26 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Integer input indicating total number of customers.
+		int totalNumberOfCustomers = scan.nextInt();
 		
+		// For loop for all information of each customer.
+		for (int i = 0; i < totalNumberOfCustomers; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numberOfItemsBought = scan.nextInt();
+			
+			// Nested for loop for each type of item bought by given customer.
+			double totalCostOfItemsBought = 0;
+			for (int n = 0; n < numberOfItemsBought; i++) {
+				int integerQuantityofItem = scan.nextInt();
+				String nameOfItem = scan.next();
+				double priceOfTheItem = scan.nextDouble();
+				totalCostOfItemsBought = totalCostOfItemsBought + priceOfTheItem;
+			}
+			// Print results for each given customer.
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + totalCostOfItemsBought);
+		}
+	
 	}
 }
